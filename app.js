623ff6d5c0912fdfc6f161a0b0bf1771f8f4862b
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-16 19:43:15 -0700

    Added fav icon
    Added fav icon in "images" folder.

---
+++ app.js
@@ -0,0 +1,10 @@
+// Set initial count.
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (orange) {
+    console.log(orange);
+});
