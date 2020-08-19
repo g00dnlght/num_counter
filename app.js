653a43b14a1b719c2086c01959f73a94aad80477
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-18 19:25:38 -0700

    Formatted using prettier
    Formatted using prettier

--- app.js
+++ app.js
@@ -2,30 +2,29 @@
 let count = 0;
 
 // Select value and buttons
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (btn) { // "btn" can be anything.
-    btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.classList;
-        if(styles.contains('decrease')){
-            count--;
-        }
-        else if(styles.contains('increase')){
-            count++;
-        }
-        else{
-            count = 0;
-        }
-        if(count > 0){
-            value.style.color = 'limegreen';
-        }
-        if(count < 0){
-            value.style.color = 'deeppink';
-        } 
-        if(count === 0){
-            value.style.color = 'whitesmoke';
-        }
-        value.textContent = count;
-    });
+btns.forEach(function (btn) {
+  // "btn" can be anything.
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "limegreen";
+    }
+    if (count < 0) {
+      value.style.color = "deeppink";
+    }
+    if (count === 0) {
+      value.style.color = "whitesmoke";
+    }
+    value.textContent = count;
+  });
 });
